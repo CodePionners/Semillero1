00bdd2d73a82c10fac52d13e98ecdf62840b7ed2
CodePionners <auntasaavedra@gmail.com>
2025-05-25 06:07:58 -0500

    Ejecucion 4

--- src/main/java/org/sistemadegestiondelesionescutaneas/SalcApplication.java
+++ src/main/java/org/sistemadegestiondelesionescutaneas/SalcApplication.java
@@ -2,10 +2,24 @@ package org.sistemadegestiondelesionescutaneas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// Importar las clases de autoconfiguración a excluir
+import org.springframework.boot.actuate.autoconfigure.audit.AuditEventsEndpointAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.beans.BeansEndpointAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.cache.CachesEndpointAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.condition.ConditionsReportEndpointAutoConfiguration; // Nueva importación
+import org.springframework.boot.actuate.autoconfigure.health.HealthEndpointAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.info.InfoEndpointAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        AuditEventsEndpointAutoConfiguration.class,       // Exclusión anterior
+        BeansEndpointAutoConfiguration.class,           // Exclusión anterior
+        CachesEndpointAutoConfiguration.class,          // Exclusión anterior
+        HealthEndpointAutoConfiguration.class,          // Exclusión anterior
+        InfoEndpointAutoConfiguration.class,            // Exclusión anterior
+        ConditionsReportEndpointAutoConfiguration.class // Nueva exclusión añadida aquí
+})
 public class SalcApplication {
 
     public static void main(String[] args) {
